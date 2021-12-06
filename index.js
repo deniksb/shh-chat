df079e9b4493b517051f9a670de5118ad20886d8
deniksb <58732525+deniksb@users.noreply.github.com>
2021-12-07 00:39:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,25 @@ const path = require('path')
 
 app.use(express.static(path.join(__dirname + '/public')))
 
+//saving last 10 messages
+let savedMessages = {};
+
 io.on('connection', socket => {
   console.log('Some client connected')
-
+  for(let i=0;i<savedMessages.length;i++){
+    io.emit('chat', savedMessages[i]);
+  }
 
   socket.on('chat', message => {
     console.log(message)
+    if(savedMessages.length <= 10){
+      savedMessages.push(message);
+    }
+    else {
+      savedMessages.shift();
+      savedMessages.push(message);
+    }
+    
     io.emit('chat', message)
   })
 })
